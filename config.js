d227ea859da8b44ba21c17ad01998d5128105eac
MAO DONG <maodong@MAOs-Mac-mini-7.local>
2026-02-18 20:21:31 +1100

    config: add Apps Script API URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const SB_CONFIG = {
   SHEET_ID: '1fyAdMu8RwcIOXBGyUhK3qqS8r-IOFVRFUokRLLaT41I',
-  API_URL: '', // Apps Script 部署后填入
+  API_URL: 'https://script.google.com/macros/s/AKfycbxlefKCj02QG2Y5Z3Ar3VN2YzD4bwAuyuA7QpYiQf0XYY9tVLN-snn6oJw0O9-ae0Q/exec',
   API_KEY: 'sb-oney-2026'
 };
